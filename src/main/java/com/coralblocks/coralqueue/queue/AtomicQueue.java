2c364a4ffce9b954f7c76e3e05aad4fd27c71fb8
Sergio Oliveira Jr <soliveira@Sergios-MacBook-Air.local>
2024-10-23 05:52:26 -0300

    Adding working AtomicQueue

---
+++ src/main/java/com/coralblocks/coralqueue/queue/AtomicQueue.java
@@ -0,0 +1,195 @@
+/* 
+ * Copyright 2024 (c) CoralBlocks - http://www.coralblocks.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package com.coralblocks.coralqueue.queue;
+
+import com.coralblocks.coralqueue.util.Builder;
+import com.coralblocks.coralqueue.util.MathUtils;
+import com.coralblocks.coralqueue.util.PaddedAtomicLong;
+
+/**
+ * An implementation of {@link Queue} that uses <i>memory barriers</i> to synchronize producer and consumer sequences.
+ *
+ * @param <E> The mutable transfer object to be used by this queue
+ */
+public class AtomicQueue<E> implements Queue<E> {
+	
+	private final static int DEFAULT_CAPACITY = 1024;
+
+	private final int capacity;
+	private final int capacityMinusOne;
+	private final E[] data;
+	private long lastOfferedSeq = -1;
+	private long lastPolledSeq = -1;
+	private long pollCount = 0;
+	private long maxSeqBeforeWrapping;
+	private final PaddedAtomicLong offerSequence = new PaddedAtomicLong(-1);
+	private final PaddedAtomicLong pollSequence = new PaddedAtomicLong(-1);
+	
+	private final Builder<E> builder;
+
+	/**
+	 * Creates an <code>AtomicQueue</code> with the given capacity using the given {@link Builder} to populate it.
+	 * 
+	 * @param capacity the capacity of the <code>AtomicQueue</code>
+	 * @param builder the {@link Builder} used to populate the <code>AtomicQueue</code>
+	 */
+	@SuppressWarnings("unchecked")
+	public AtomicQueue(int capacity, Builder<E> builder) {
+		MathUtils.ensurePowerOfTwo(capacity);
+		this.capacity = capacity;
+		this.capacityMinusOne = capacity - 1;
+		this.data = (E[]) new Object[capacity];
+		for (int i = 0; i < capacity; i++) {
+			this.data[i] = builder.newInstance();
+		}
+		this.maxSeqBeforeWrapping = calcMaxSeqBeforeWrapping();
+		this.builder = builder;
+	}
+
+	/**
+	 * Creates an <code>AtomicQueue</code> with the default capacity (1024) using the given {@link Builder} to populate it.
+	 * 
+	 * @param builder the {@link Builder} used to populate the <code>AtomicQueue</code>
+	 */
+	public AtomicQueue(Builder<E> builder) {
+		this(DEFAULT_CAPACITY, builder);
+	}
+
+	/**
+	 * Creates an <code>AtomicQueue</code> with the given capacity using the given class to populate it.
+	 * 
+	 * @param capacity the capacity of the <code>AtomicQueue</code>
+	 * @param klass the class used to populate the <code>AtomicQueue</code>
+	 */
+	public AtomicQueue(int capacity, Class<E> klass) {
+		this(capacity, Builder.createBuilder(klass));
+	}
+	
+	/**
+	 * Creates an <code>AtomicQueue</code> with the default capacity (1024) using the given class to populate it.
+	 * 
+	 * @param klass the class used to populate the <code>AtomicQueue</code>
+	 */
+	public AtomicQueue(Class<E> klass) {
+		this(Builder.createBuilder(klass));
+	}
+	
+	@Override
+	public final void clear() {
+		lastOfferedSeq = -1;
+		lastPolledSeq = -1;
+		pollCount = 0;
+		offerSequence.set(-1);
+		pollSequence.set(-1);
+		maxSeqBeforeWrapping = calcMaxSeqBeforeWrapping();
+	}
+	
+	private final long calcMaxSeqBeforeWrapping() {
+		return pollSequence.get() + capacity;
+	}
+	
+	public final Builder<E> getBuilder() {
+		return builder;
+	}
+
+	@Override
+	public final E nextToDispatch() {
+		if (++lastOfferedSeq > maxSeqBeforeWrapping) {
+			// this would wrap the buffer... calculate the new one...
+			this.maxSeqBeforeWrapping = calcMaxSeqBeforeWrapping();
+			if (lastOfferedSeq > maxSeqBeforeWrapping) {
+				lastOfferedSeq--;
+				return null;				
+			}
+		}
+		return data[(int) (lastOfferedSeq & capacityMinusOne)];
+	}
+	
+	@Override
+	public final E nextToDispatch(E swap) {
+		E val = nextToDispatch();
+		if (val == null) return null;
+		data[(int) (lastOfferedSeq & capacityMinusOne)] = swap;
+		return val;
+	}
+	
+	@Override
+	public final void flush(boolean lazySet) {
+		if (lazySet) {
+			offerSequence.lazySet(lastOfferedSeq);
+		} else {
+			offerSequence.set(lastOfferedSeq);
+		}
+	}
+	
+	@Override
+	public final void flush() {
+		// don't call flush(false) to save one method call (more performance)
+		offerSequence.set(lastOfferedSeq); // no lazySet by default...
+	}
+
+	@Override
+	public final long availableToPoll() {
+		return offerSequence.get() - lastPolledSeq;
+	}
+	
+	@Override
+	public final E poll() {
+		pollCount++;
+		return data[(int) (++lastPolledSeq & capacityMinusOne)];
+	}
+	
+	@Override
+	public final void replace(E newVal) {
+		data[(int) (lastPolledSeq & capacityMinusOne)] = newVal;
+	}
+	
+	@Override
+	public final E peek() {
+		return data[(int) (lastPolledSeq & capacityMinusOne)];
+	}
+
+	@Override
+	public final void donePolling(boolean lazySet) {
+		if (lazySet) {
+			pollSequence.lazySet(lastPolledSeq);
+		} else {
+			pollSequence.set(lastPolledSeq);
+		}
+		pollCount = 0;
+	}
+	
+	@Override
+	public final void rollBack() {
+		rollBack(pollCount);
+	}
+	
+	@Override
+	public final void rollBack(long count) {
+		if (count < 0 || count > pollCount) {
+			throw new RuntimeException("Invalid rollback request! polled=" + pollCount + " requested=" + count);
+		}
+		lastPolledSeq -= count;
+		pollCount -= count;
+	}
+	
+	@Override
+	public final void donePolling() {
+		// don't call donePolling(false) to save one method call (more performance)
+		pollSequence.set(lastPolledSeq); // no lazySet by default...
+		pollCount = 0;
+	}
+}
---
+++ src/main/java/com/coralblocks/coralqueue/util/PaddedAtomicLong.java
@@ -0,0 +1,67 @@
+/* 
+ * Copyright 2024 (c) CoralBlocks - http://www.coralblocks.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package com.coralblocks.coralqueue.util;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * A padded <code>AtomicLong</code> that occupies a whole CPU cache line (64Kb) so that
+ * two different sequences (from producer and from consumer) do not mess with each other inside the CPU cache. 
+ */
+public class PaddedAtomicLong extends AtomicLong {
+	
+	private static final long VALUE_LONG = 19760120L;
+	
+	public volatile long value1 = VALUE_LONG + 0;
+	public volatile long value2 = VALUE_LONG + 1;
+	public volatile long value3 = VALUE_LONG + 2;
+	public volatile long value4 = VALUE_LONG + 3;
+	public volatile long value5 = VALUE_LONG + 4;
+	public volatile long value6 = VALUE_LONG + 5;
+	
+	/*
+	 * Why do we only need 6 longs and not 7 ???
+	 * 
+	 * That's because each object has at least a header with 8 bytes.
+	 * 
+	 * So to complete a 64bytes cache line, we have:
+	 * 
+	 *  - the header (8 bytes)
+	 *  - our sequence (8 bytes)
+	 *  - 6 longs (6 x 8 = 48 bytes)
+	 *  
+	 *  TOTAL: 64 bytes
+	 */
+	
+	/**
+	 * Creates a new <code>PaddedAtomicLong</code> by calling <code>super(value)</code>.
+	 * 
+	 * @param value the value of the <code>AtomicLong</code> superclass.
+	 */
+	public PaddedAtomicLong(final long value) {
+		super(value);
+	}
+
+	/**
+	 * This method has no purpose. It is here just to prevent HotSpot optimization and code removal.
+	 * 
+	 * @return the total of all values inside the object
+	 */
+	public long getTotal() {
+		// try to prevent hotspot optimization...
+		return value1 + value2 + value3 + value4 + value5 + value6;
+	}
+}
