f3a84ece03b27d0c2be3a7fa56dfcb44a4f6885b
Sergio Oliveira <sergio.oliveira@coralblocks.com>
2024-10-27 19:19:30 -0300

    JavaDoc comments

--- src/main/java/com/coralblocks/coralqueue/broadcaster/BroadcasterDelegateQueue.java
+++ src/main/java/com/coralblocks/coralqueue/broadcaster/BroadcasterDelegateQueue.java
@@ -17,14 +17,43 @@ package com.coralblocks.coralqueue.broadcaster;
 
 import com.coralblocks.coralqueue.queue.Queue;
 
+/**
+ * A delegate so that a {@link Broadcaster} can be used as a {@link Queue}.
+ *
+ * @param <E> The mutable transfer object to be used by this broadcaster
+ */
 public class BroadcasterDelegateQueue<E> implements Queue<E> {
 	
 	private final Broadcaster<E> broadcaster;
-	private final int index;
+	private final int consumerIndex;
 	
-	public BroadcasterDelegateQueue(Broadcaster<E> broadcaster, int index) {
+	/**
+	 * Creates a <code>BroadcasterDelegateQueue</code>.
+	 * 
+	 * @param broadcaster the broadcaster to delegate to
+	 * @param consumerIndex the consumerIndex to use
+	 */
+	public BroadcasterDelegateQueue(Broadcaster<E> broadcaster, int consumerIndex) {
 		this.broadcaster = broadcaster;
-		this.index = index;
+		this.consumerIndex = consumerIndex;
+	}
+	
+	/**
+	 * Return the consumer index being used by this delegate
+	 * 
+	 * @return the consumer index
+	 */
+	public final int getConsumerIndex() {
+		return consumerIndex;
+	}
+	
+	/**
+	 * Return the broadcaster used as the deledage
+	 * 
+	 * @return the broadcaster used as the delegate
+	 */
+	public final Broadcaster<E> getDelegate() {
+		return broadcaster;
 	}
 	
 	@Override
@@ -54,12 +83,12 @@ public class BroadcasterDelegateQueue<E> implements Queue<E> {
 
 	@Override
     public long availableToPoll() {
-	    return broadcaster.availableToPoll(index);
+	    return broadcaster.availableToPoll(consumerIndex);
     }
 
 	@Override
     public E poll() {
-		return broadcaster.poll(index);
+		return broadcaster.poll(consumerIndex);
     }
 	
 	@Override
@@ -69,26 +98,26 @@ public class BroadcasterDelegateQueue<E> implements Queue<E> {
 
 	@Override
     public void donePolling(boolean lazySet) {
-		broadcaster.donePolling(index, lazySet);
+		broadcaster.donePolling(consumerIndex, lazySet);
     }
 
 	@Override
     public void donePolling() {
-		broadcaster.donePolling(index);
+		broadcaster.donePolling(consumerIndex);
     }
 
 	@Override
     public void rollBack() {
-		broadcaster.rollBack(index);
+		broadcaster.rollBack(consumerIndex);
     }
 
 	@Override
     public void rollBack(long items) {
-		broadcaster.rollBack(index, items);
+		broadcaster.rollBack(consumerIndex, items);
     }
 
 	@Override
     public E peek() {
-	    return broadcaster.peek(index);
+	    return broadcaster.peek(consumerIndex);
     }
 }
--- src/main/java/com/coralblocks/coralqueue/broadcaster/Consumer.java
+++ src/main/java/com/coralblocks/coralqueue/broadcaster/Consumer.java
@@ -15,37 +15,68 @@
  */
 package com.coralblocks.coralqueue.broadcaster;
 
+/**
+ * A consumer to receive messages from this broadcaster. 
+ *
+ * @param <E> The mutable transfer object to be used by this broadcaster
+ */
 public class Consumer<E> {
 	
-	private final int index;
+	private final int consumerIndex;
 	private final Broadcaster<E> broadcaster;
 	
-	Consumer(Broadcaster<E> broadcaster, int index) {
-		this.index = index;
+	Consumer(Broadcaster<E> broadcaster, int consumerIndex) {
 		this.broadcaster = broadcaster;
+		this.consumerIndex = consumerIndex;
 	}
 	
+	/**
+	 * Return the consumer index
+	 * 
+	 * @return the consumer index 
+	 */
 	public final int getIndex() {
-		return index;
+		return consumerIndex;
 	}
 	
+	/**
+	 * Delegate to the broadcaster
+	 * 
+	 * @return the number of available objects that can be polled
+	 */
 	public final long availableToPoll() {
-		return broadcaster.availableToPoll(index);
+		return broadcaster.availableToPoll(consumerIndex);
 	}
 	
+	/**
+	 * Delegate to the broadcaster
+	 * 
+	 * @return the object polled
+	 */
 	public final E poll() {
-		return broadcaster.poll(index);
+		return broadcaster.poll(consumerIndex);
 	}
 	
+	/**
+	 * Delegate to the broadcaster
+	 * 
+	 * @param lazySet false to signal to the producer immediately
+	 */
 	public final void donePolling(boolean lazySet) {
-		broadcaster.donePolling(index, lazySet);
+		broadcaster.donePolling(consumerIndex, lazySet);
 	}
 	
+	/**
+	 * Delegate to the broadcaster
+	 */
 	public final void donePolling() {
-		broadcaster.donePolling(index);
+		broadcaster.donePolling(consumerIndex);
 	}
 	
+	/**
+	 * Disable this consumer by delegating to the <code>disableConsumer(int)</code> method of this broadcaster
+	 */
 	public final void disable() {
-		broadcaster.disableConsumer(index);
+		broadcaster.disableConsumer(consumerIndex);
 	}
 }
