a0f18a71004eca779fe973786b4ae2349e1b5e06
Sergio Oliveira <sergio.oliveira@coralblocks.com>
2024-10-30 07:59:04 -0300

    Javadoc comments

--- src/main/java/com/coralblocks/coralqueue/waitstrategy/SpinParkWaitStrategy.java
+++ src/main/java/com/coralblocks/coralqueue/waitstrategy/SpinParkWaitStrategy.java
@@ -18,9 +18,11 @@ package com.coralblocks.coralqueue.waitstrategy;
 import java.util.concurrent.locks.LockSupport;
 
 /**
- * This wait strategy first busy-spins then parks with backing off if enabled. Also supports a maximum park time (default is 1 millisecond).
+ * <p>A wait strategy that busy spins for some cycles, then parks 1 nanosecond by calling <code>LockSupport.parkNanos(1L);</code>.
+ * It can also back off by incrementing its park time by 1 nanosecond until it reaches a maximum park time.
+ * Its string type for the factory method {@link WaitStrategy#getWaitStrategy(String)} is "spinPark".</p>
  * 
-* 
+ * <p>NOTE: You can optionally pass -DcoralQueueBlockCount=true to count the total number of blocks.</p>
  */
 public class SpinParkWaitStrategy implements WaitStrategy {
 
@@ -37,24 +39,50 @@ public class SpinParkWaitStrategy implements WaitStrategy {
 	
 	private final BlockCount blockCount = new BlockCount();
 
+	/**
+	 * Creates a <code>SpinParkWaitStrategy</code>.
+	 * 
+	 * @param spinCount the number of cycles to busy spin before starting to park
+	 * @param parkBackOff true to support backing off by increasing the park time
+	 * @param maxParkTime the max park time in nanoseconds if park backing off is enabled
+	 */
 	public SpinParkWaitStrategy(final int spinCount, final boolean parkBackOff, final long maxParkTime) {
 		this.spinCount = spinCount;
 		this.parkBackOff = parkBackOff;
 		this.maxParkTime = maxParkTime;
 	}
 
+	/**
+	 * Creates a <code>SpinParkWaitStrategy</code> with a default spin count of 1_000_000 and a default max park time (for backing off) of 1_000_000 nanoseconds.
+	 * 
+	 * @param parkBackOff true to use backing off
+	 */
 	public SpinParkWaitStrategy(final boolean parkBackOff) {
 		this(DEFAULT_SPIN_COUNT, parkBackOff, DEFAULT_MAX_PARK_TIME);
 	}
 	
+	/**
+	 * Creates a <code>SpinParkWaitStrategy</code> with a default spin count of 1_000_000.
+	 * 
+	 * @param parkBackOff true to support backing off
+	 * @param maxParkTime the max park time in nanoseconds if park backing off is enabled
+	 */
 	public SpinParkWaitStrategy(final boolean parkBackOff, final long maxParkTime) {
 		this(DEFAULT_SPIN_COUNT, parkBackOff, maxParkTime);
 	}
 
+	/**
+	 * Creates a <code>SpinParkWaitStrategy</code> without backing off (will keep parking 1 nanosecond after the spin count).
+	 * 
+	 * @param spinCount the number of cycles to busy spin before starting to park
+	 */
 	public SpinParkWaitStrategy(final int spinCount) {
 		this(spinCount, DEFAULT_BACK_OFF, DEFAULT_MAX_PARK_TIME);
 	}
 
+	/**
+	 * Creates a <code>SpinParkWaitStrategy</code> with a default spin count of 1_000_000 and without backing off (will keep parking 1 nanosecond after the spin count).
+	 */
 	public SpinParkWaitStrategy() {
 		this(DEFAULT_SPIN_COUNT);
 	}
--- src/main/java/com/coralblocks/coralqueue/waitstrategy/SpinYieldParkWaitStrategy.java
+++ src/main/java/com/coralblocks/coralqueue/waitstrategy/SpinYieldParkWaitStrategy.java
@@ -18,11 +18,11 @@ package com.coralblocks.coralqueue.waitstrategy;
 import java.util.concurrent.locks.LockSupport;
 
 /**
- * This wait strategy first busy spins, then yields, then sleep. You can configure each component by passing a spinCount and yieldCount.
- * It optionally supports backing-off for the LockSupport.parkNanos method, increasing the sleep time by one nanosecond until the reset
- * method is called. It also supports a maximum sleep time (default is 1 millisecond).
+ * <p>A wait strategy that busy spins for some cycles, then yields for some cycles, then parks 1 nanosecond by calling <code>LockSupport.parkNanos(1L)</code>.
+ * It can also back off by incrementing its park time by 1 nanosecond until it reaches a maximum park time.
+ * Its string type for the factory method {@link WaitStrategy#getWaitStrategy(String)} is "spinYieldPark".</p>
  * 
-* 
+ * <p>NOTE: You can optionally pass -DcoralQueueBlockCount=true to count the total number of blocks.</p>
  */
 public class SpinYieldParkWaitStrategy implements WaitStrategy {
 
@@ -41,6 +41,14 @@ public class SpinYieldParkWaitStrategy implements WaitStrategy {
 	
 	private final BlockCount blockCount = new BlockCount();
 
+	/**
+	 * Creates a <code>SpinYieldParkWaitStrategy</code>.
+	 * 
+	 * @param spinCount the number of cycles to busy spin before starting to yield
+	 * @param yieldCount the number of cycles to yield before starting to park
+	 * @param parkBackOff true to support backing off by increasing the park time
+	 * @param maxParkTime the max park time in nanoseconds if park backing off is enabled
+	 */
 	public SpinYieldParkWaitStrategy(final int spinCount, final int yieldCount, final boolean parkBackOff, final long maxParkTime) {
 		this.spinCount = spinCount;
 		this.yieldCount = yieldCount + spinCount;
@@ -48,22 +56,49 @@ public class SpinYieldParkWaitStrategy implements WaitStrategy {
 		this.maxParkTime = maxParkTime;
 	}
 	
+	/**
+	 * Creates a <code>SpinYieldParkWaitStrategy</code> with the default max park time of 1_000_000 nanoseconds (if backing off is enabled).
+	 * 
+	 * @param spinCount the number of cycles to busy spin before starting to yield
+	 * @param yieldCount the number of cycles to yield before starting to park
+	 * @param parkBackOff true to support backing off by increasing the park time
+	 */
 	public SpinYieldParkWaitStrategy(final int spinCount, final int yieldCount, final boolean parkBackOff) {
 		this(spinCount, yieldCount, parkBackOff, DEFAULT_MAX_PARK_TIME);
 	}
 	
+	/**
+	 * Creates a <code>SpinYieldParkWaitStrategy</code> with the default spin count of 1_000_000 and the default yield count of 1_000.
+	 * 
+	 * @param parkBackOff true to support backing off by increasing the park time
+	 * @param maxParkTime the max park time in nanoseconds if park backing off is enabled
+	 */
 	public SpinYieldParkWaitStrategy(final boolean parkBackOff, final long maxParkTime) {
 		this(DEFAULT_SPIN_COUNT, DEFAULT_YIELD_COUNT, parkBackOff, maxParkTime);
 	}
 
+	/**
+	 * Creates a <code>SpinYieldParkWaitStrategy</code> with the default spin count of 1_000_000, the default yield count of 1_000 and the default max park time of 1_000_000 nanoseconds.
+	 * 
+	 * @param parkBackOff true to support backing off by increasing the park time
+	 */
 	public SpinYieldParkWaitStrategy(final boolean parkBackOff) {
 		this(DEFAULT_SPIN_COUNT, DEFAULT_YIELD_COUNT, parkBackOff, DEFAULT_MAX_PARK_TIME);
 	}
 
+	/**
+	 * Creates a <code>SpinYieldParkWaitStrategy</code> without any backing off from park.
+	 * 
+	 * @param spinCount the number of cycles to busy spin before starting to yield
+	 * @param yieldCount the number of cycles to yield before starting to park
+	 */
 	public SpinYieldParkWaitStrategy(final int spinCount, final int yieldCount) {
 		this(spinCount, yieldCount, DEFAULT_BACK_OFF, DEFAULT_MAX_PARK_TIME);
 	}
 
+	/**
+	 * Creates a <code>SpinYieldParkWaitStrategy</code> with the default spin count of 1_000_000, the default yield count of 1_000 and without any backing off from park.
+	 */
 	public SpinYieldParkWaitStrategy() {
 		this(DEFAULT_SPIN_COUNT, DEFAULT_YIELD_COUNT, DEFAULT_BACK_OFF, DEFAULT_MAX_PARK_TIME);
 	}
